a319ced3569e655f82d7ee2e7f94fe28d4918731
Fabian Garber <fabiangarber@gmx.at>
2025-02-08 21:15:44 +0100

    Remove outdated dotfiles and replace with new ones

---
+++ fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+alias du="du -h"
+alias df="df -h"
+alias dnf='sudo dnf'
+alias ll='ls -lh --color=auto'
+alias du='du -h'
+alias open='xdg-open'
+alias vim='nvim'
+alias vi='nvim'
+alias docs='cd /home/fabi/Documents'
+alias torrents='cd /home/fabi/Videos/torrents'
+alias ..='cd ..'
+alias cs='cd;ls'
+alias grep='grep --color=auto'
+alias bc='bc -l'
+alias diff='colordiff'
+alias h='history'
+alias j='jobs -l'
+alias dmesg='sudo dmesg'
+alias rpi='192.168.0.159'
+alias da='/home/fabi/Documents/diplomarbeit'
+
+set fish_greeting
+
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+
---
+++ nvim/init.vim
@@ -0,0 +1,11 @@
+:set nu rnu
+call plug#begin()
+
+" List your plugins here
+Plug 'ludovicchabant/vim-gutentags'
+Plug 'Mofiqul/dracula.nvim'
+Plug 'ellisonleao/gruvbox.nvim'
+
+call plug#end()
+
+:colorscheme gruvbox
